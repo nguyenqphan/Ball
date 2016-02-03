48722e0619060a463064f8548de710a6ad662c9e
Nguyen Phan <nqphan212@gmail.com>
2016-02-03 12:19:50 -0500

    Randomly choose skybox and ball materials

---
+++ Assets/__Scripts/PlayerMaterials.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMaterials : MonoBehaviour {
+
+	public Material[] materials;
+	private Renderer renderMaterial;
+
+	void Awake()
+	{
+		renderMaterial = GetComponent<Renderer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		renderMaterial.material = materials[randomIndex()];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	int randomIndex()
+	{
+		return Random.Range(0, materials.Length);
+	}
+}
---
+++ Assets/__Scripts/SkyboxMaterials.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxMaterials : MonoBehaviour {
+
+	public Material[] materials;
+	private Skybox skybox;
+	// Use this for initialization
+
+	void Awake()
+	{
+		skybox = GetComponent<Skybox>();
+	}
+	void Start () 
+	{
+		skybox.material = materials[4];	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	int randomIndex()
+	{
+		return Random.Range(0, materials.Length);
+	}
+}
+	
