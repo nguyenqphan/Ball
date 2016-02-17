f87ef9eb91554fe3bed94867782f2bedffc128cf
Nguyen Phan <nqphan212@gmail.com>
2016-02-17 16:18:22 -0500

    Delete some unused assets

--- Assets/__Scripts/Notes/Ideas.cs
+++ Assets/__Scripts/Notes/Ideas.cs
@@ -6,8 +6,8 @@ public class Ideas : MonoBehaviour {
 /*  Cubes are flying in from somewhere in the world
  *	Turn The plane background
  *	Cube size change after being hit by the ball
+ *  Create big diamond for 10 and 20 points for playing more than 30 without QUITING the game.
  *  
- * 
  * 
  * 
  * 
